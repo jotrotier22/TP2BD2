a7d9698b865450df400bce4ea22c3766e703541b
martingel13 <70383605+martingel13@users.noreply.github.com>
2022-03-25 16:17:16 -0400

    formulaire add admin

---
+++ TP02-HugoLandWPF-Editeur de Mondes(Base)/MapEditor/AddAdminWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MapEditor
+{
+    /// <summary>
+    /// Logique d'interaction pour AddAdminWindow.xaml
+    /// </summary>
+    public partial class AddAdminWindow : Window
+    {
+        public AddAdminWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_RechercherParID_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_RechercherParPseudo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
